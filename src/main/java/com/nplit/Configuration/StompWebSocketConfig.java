ff4553b6a3cc24214d946040e8f6faea3940e966
secretj <qkrwlsgud890@gmail.com>
2022-01-02 17:58:15 +0900

    주석정리

--- src/main/java/com/nplit/Configuration/StompWebSocketConfig.java
+++ src/main/java/com/nplit/Configuration/StompWebSocketConfig.java
@@ -10,9 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-	// endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
-	// Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
-	// origins를 개발 도메인으로 변경하니 잘 동작하였다.
+	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/stomp/chat").setAllowedOrigins("http://localhost:8080").withSockJS();
